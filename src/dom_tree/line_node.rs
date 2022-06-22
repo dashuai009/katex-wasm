84482ea2bf26964e35639bdc67149f4c0654439f
dashuai009 <1525836294@qq.com>
2022-06-22 21:34:47 +0800

    line_node

---
+++ src/dom_tree/line_node.rs
@@ -0,0 +1,54 @@
+use crate::dom_tree::css_style::CssStyle;
+use crate::utils::{escape, make_em};
+use crate::{path_get, scriptFromCodepoint, VirturalNode};
+use js_sys::Array;
+use std::collections::HashMap;
+use std::sync::Mutex;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct LineNode {
+    attributes: HashMap<String, String>,
+}
+
+#[wasm_bindgen]
+impl LineNode {
+    #[wasm_bindgen(constructor)]
+    pub fn new(attributes: js_sys::Object) -> LineNode {
+        let mut res = HashMap::new();
+        for (k, v) in js_sys::Object::keys(&attributes)
+            .iter()
+            .zip(js_sys::Object::values(&attributes).iter())
+        {
+            res.insert(k.as_string().unwrap(), v.as_string().unwrap());
+        }
+        // this.attributes = attributes || {};
+        LineNode { attributes: res }
+    }
+
+    pub fn toNode(self) -> web_sys::Node {
+        let svgNS = "http://www.w3.org/2000/svg";
+
+        let document = web_sys::window().expect("").document().expect("");
+        let node = document.create_element_ns(Some(svgNS), "line").expect("");
+
+        // Apply attributes
+        for (k, v) in self.attributes.iter() {
+            web_sys::Element::set_attribute(&node, k.as_str(), v.as_str());
+        }
+
+        return web_sys::Node::from(node);
+    }
+
+    pub fn toMarkup(&self) -> String {
+        let mut markup: String = "<line".to_string();
+
+        for (k, v) in self.attributes.iter() {
+            markup.push_str(format!(" {}:{}", k, v).as_str());
+        }
+
+        markup.push_str("/>");
+
+        return markup;
+    }
+}
--- src/dom_tree/mod.rs
+++ src/dom_tree/mod.rs
@@ -2,3 +2,4 @@ mod css_style;
 mod img;
 mod path_node;
 pub mod symbol_node;
+mod line_node;
