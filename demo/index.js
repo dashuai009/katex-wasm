e67ad33078360d62e3b3b064e2d097c8df9cbfed
dashuai009 <1525836294@qq.com>
2022-10-12 22:30:47 +0800

    demo

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,21 @@
-import * as wasm from "hello-wasm-pack";
-import * as katex from "katex-wasm";
-wasm.greet();
+import * as katex_wasm from "katex-wasm";
+import * as katex from "katex";
 
-let s = "a^2+b^2=1";
-console.log(katex)
-let t = katex.renderToString(s,{displayMode: true, throwOnError: false});
-console.log(t);
+const math_str = [
+    "a^2+b^2=1",
+    "E=mc^2",
+    "\\\"{A}"
+]
+
+let math_wasm = document.getElementById("math-wasm");
+let math_katex = document.getElementById("math-katex")
+for (let s of math_str) {
+    let t = katex_wasm.renderToString(s, {displayMode: true, throwOnError: false});
+    let d = document.createElement("div");
+    d.innerHTML = t;
+    math_wasm.append(d);
+
+    let d2 = document.createElement("div");
+    katex.render(s, d2, {displayMode: true, throwOnError: false});
+    math_katex.append(d2);
+}
